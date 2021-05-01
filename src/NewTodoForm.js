b54c2520b513a160b5c5e1fad7be1a4bbefe002c
Scooter <scott.skrobel@gmail.com>
2021-05-01 17:59:31 -0600

    NewtodoForm now adds item todo component

--- src/NewTodoForm.js
+++ src/NewTodoForm.js
@@ -3,3 +3,44 @@ import React, { Component } from 'react';
  *  When submitted, a new Todo component should be created.
  */
 
+class NewTodoForm extends Component {
+  constructor(props) {
+    super(props);
+    // deffaukt todo is an empty string
+    this.state = { newTodoItem: "" };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(evt) {
+    // runs on every keystroke event
+    this.setState({
+      [evt.target.name]: evt.target.value
+    });
+  }
+
+  handleSubmit(evt) {
+    evt.preventDefault();
+    this.props.createTodo(this.state.newTodoItem);
+    this.setState({ newTodoItem: "" });
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label htmlFor='newTodo'></label>
+        <input
+          type='text'
+          placeholder='New Todo'
+          id='newTodo'
+          name='newTodoItem'
+          value={this.state.newTodoItem}
+          onChange={this.handleChange}
+        />
+        <button>Add Todo</button>
+      </form>
+    );
+  }
+}
+
+export default NewTodoForm;
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import NewTodoForm from './NewTodoForm';
 import Todo from './Todo'
 /** renders NewTodoForm component and the list of todo components */
 // state containing all todos in this component thereffor add constructor 
@@ -8,15 +9,24 @@ class TodoList extends Component {
     this.state = {
       list: [{ todo: "Get a jerb" }, { todo: "Get a hairscut" }]
     };
+    this.create = this.create.bind(this);
+  }
+  // Takes newTodo object and adds into todos state
+  create(newTodo) {
+    this.setState({
+      list: [...this.state.list, newTodo]
+    });
   }
 
   render() {
-    const list = this.state.list.map(item => (
-      <Todo todoItem={item.todo} />
-    ));
+    const todoList = this.state.list.map(item => {
+      return <Todo todoItem={item.todo} />;
+    });
     return (
       <div>
-        {list}
+        <NewTodoForm createTodo={this.create} />
+        {todoList}
+
       </div>
     );
   }
